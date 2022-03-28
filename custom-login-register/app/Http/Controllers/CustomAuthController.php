9f836f3d77bc3b1100a5b82f17ba185a7ed1c6b9
shafikcmt <mdshafiqulislam822@gmail.com>
2022-03-28 11:01:59 +0530

    Register and Login Page Design

--- custom-login-register/app/Http/Controllers/CustomAuthController.php
+++ custom-login-register/app/Http/Controllers/CustomAuthController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class CustomAuthController extends Controller
 {
-    //
+    public function login(){
+        return view("auth.login");
+    }
+    public function registration(){
+        return view("auth.registration");
+    }
 }
---
+++ custom-login-register/resources/views/auth/login.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Registration</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Registration</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="/action_page.php">
+                            <div class="mb-3 mt-3">
+                                <label for="roll" class="form-label">Roll:</label>
+                                <input type="text" class="form-control" id="roll" placeholder="Enter roll"
+                                    name="email">
+                            </div>
+                           
+                            <div class="mb-3">
+                                <label for="pwd" class="form-label">Password:</label>
+                                <input type="password" class="form-control" id="pwd" placeholder="Enter password"
+                                    name="pswd">
+                            </div>
+                            
+                            <button name="login" type="submit" class="btn btn-primary">Login</button>
+                        </form>
+                    </div>
+                    <div class="card-footer">
+                        <h3>Geeta Technical HUB</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
