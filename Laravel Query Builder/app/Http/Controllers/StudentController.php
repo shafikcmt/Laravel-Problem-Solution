339712b3b859a46d80fe58013d87b4bc7246d0ff
Your Name <you@example.com>
2022-04-01 04:58:40 +0600

    project Created AdminUserAuthSystem

--- Laravel Query Builder/app/Http/Controllers/StudentController.php
+++ Laravel Query Builder/app/Http/Controllers/StudentController.php
@@ -9,8 +9,12 @@ class StudentController extends Controller
 {
     public function show(){
         $students = '';
+        $students1 = '';
+        $students2 = '';
         $students = DB::table('student_table')->get();
-        
-        return view('student',['students'=>$students]);
+        $students1 = DB::table('student_table')->first();
+        $students2 = DB::table('student_table')->where('name','Md Shafiqul Islam')->first();
+//        dd($students1);
+        return view('student',['students'=>$students,'students1'=>$students1,'students2'=>$students2]);
     }
 }
--- Laravel Query Builder/resources/views/student.blade.php
+++ Laravel Query Builder/resources/views/student.blade.php
@@ -3,11 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Query Builder in Laravel</title>
 </head>
 
 <body>
     <table width="100%" border="2" cellpadding="10">
+       <h2>Retrieving All Rows from Table</h2>
         <thead>
             <tr>
                 <th>Name</th>
@@ -23,6 +24,32 @@
                 <td>{{$stu->phone}}</td>
             </tr>
             @endforeach
+        </tbody>
+
+    </table>
+    
+     <table width="100%" border="2" cellpadding="10">
+       <h2>Retrieving Single Row/Column from Table</h2>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+            </tr>
+        </thead>
+        <tbody>
+           
+            <tr>
+                <td>{{$students1->name}}</td>
+                <td>{{$students1->email}}</td>
+                <td>{{$students1->phone}}</td>
+            </tr>
+            <tr>
+                <td>{{$students2->name}}</td>
+                <td>{{$students2->email}}</td>
+                <td>{{$students2->phone}}</td>
+            </tr>
+           
         </tbody>
 
     </table>
